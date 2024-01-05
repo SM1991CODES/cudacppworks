2deb1cd64f82b67749f3bd2e91c70627c9ed1892
smohapat <sambit.mohapatra1903@gmail.com>
2024-01-05 10:00:29 +0100

    - visual studio project

---
+++ cpp_studio_win/cpp_studio_win.cpp
@@ -0,0 +1,12 @@
+﻿// cpp_studio_win.cpp : Defines the entry point for the application.
+//
+
+#include "cpp_studio_win.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
