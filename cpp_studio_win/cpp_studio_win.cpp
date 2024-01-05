cfe7de1ffa13f20ef2291a59008400e115f815b8
smohapat <sambit.mohapatra1903@gmail.com>
2024-01-05 12:50:38 +0100

    - sync

--- cpp_studio_win/cpp_studio_win.cpp
+++ cpp_studio_win/cpp_studio_win.cpp
@@ -5,8 +5,36 @@
 
 using namespace std;
 
+/**
+* Function shows loops and break and continue
+*/
+void loops_break_continue()
+{
+	int j = 0;
+	//
+	for (int i = 0; i < 10; i++)
+	{
+		while (j < 10)
+		{
+			cout << "i = " << i << " j = " << j << endl;
+			if (j > 3)
+				break;
+			j++;
+			
+			if (j > 5)
+				continue;
+
+			cout << "j now -> " << j << endl;
+		}
+
+	}
+
+}
+
 int main()
 {
 	cout << "Hello CMake." << endl;
+	//
+	//loops_break_continue();
 	return 0;
 }
